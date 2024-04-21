a075cb7b6757aa7caa004a316ee9b2536d595db5
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-04-21 19:48:41 +0300

    Add project files.

---
+++ File/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = File.ReadAllLines("C:\\hw.txt");
+            array = array.Select(e => e.FirstSymbolsToUpper()).ToArray();
+        }
+
+
+    }
+    public static class StringExtensions
+    {
+        public static string FirstSymbolsToUpper(this string input) =>
+            input switch
+            {
+                "" => throw new Exception("This string cannot be empty"),
+                _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+            };
+    }
+}
